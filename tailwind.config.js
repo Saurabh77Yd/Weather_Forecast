cd2ca70249d6270473a1d98ff175d1c823fcdafe
Saurabh Yadav <yadavsaurabhyd@gmail.com>
2024-06-20 07:26:00 +0530

    Weather forcast done

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#e3f2fd",
+        background: "#5372f0",
+      },
+    },
+  },
+  plugins: [],
+};
